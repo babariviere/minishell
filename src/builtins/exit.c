b415f846bd68644979d6a0081cb65f4a1b7d092f
babariviere <babariviere@protonmail.com>
2017-12-13 10:41:52 +0100

    exit crime

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 14:48:57 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/12 14:49:34 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/13 10:41:37 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@ int		builtin_exit(int ac, char **av, char **envp)
 	(void)ac;
 	(void)av;
 	(void)envp;
+	
+	if (ac == 2)
+		exit(ft_atoi(av[1]));
 	exit(0);
 }
