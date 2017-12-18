aa520b1cc71a8152c517252099688550e2af6be8
babariviere <babariviere@protonmail.com>
2017-12-18 12:04:51 +0100

    cwd

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 08:56:39 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/18 09:40:06 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/18 12:02:46 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ void	sigint_handler(int sig)
 	(void)sig;
 	if (running_pid == 0)
 	{
-		ft_putchar('\r');
-		put_shell_prompt();
-		ft_putstr("  \n");
+		ft_putchar('\n');
 		put_shell_prompt();
 		ft_env_set(&environ, "?", "1", 1);
 		return ;
