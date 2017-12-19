cc9ae08060c47821e66ae6fb10a483de48ec5c96
babariviere <babariviere@protonmail.com>
2017-12-19 11:46:01 +0100

    fix signal

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 08:56:39 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/18 13:39:29 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/19 11:45:00 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	sigint_handler(int sig)
 	(void)sig;
 	if (g_running_pid == 0)
 	{
+		ft_putstr("\33[2K\r");
+		put_shell_prompt();
 		ft_putchar('\n');
 		put_shell_prompt();
 		ft_env_set(ft_env_load(), "?", "1", 1);
